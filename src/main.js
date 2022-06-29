d4fbd5a8de982b94f68848e2b0a386fe7cbbd937
Ihor Karpyn <igor82200@gmail.com>
2022-06-29 11:02:04 +0300

    added-acordion

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 'use strict';
 
-const btnOpen = document.getElementById('btnOpen');
-const btnClose = document.getElementById('btnClose');
-const modal = document.getElementById('modal');
+const btn = document.getElementById('btn');
+const acordion = document.getElementById('acordion');
 
-btnClose.addEventListener('click', () => {
-  modal.classList.add('modal--hidden');
-});
-
-btnOpen.addEventListener('click', () => {
-  modal.classList.remove('modal--hidden');
-});
+btn.addEventListener('click', () => {
+  acordion.classList.toggle('wrapper--hidden');
+})
