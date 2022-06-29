4504bce4f348227d5cd08672e5a3a4779992ee6a
Ihor Karpyn <igor82200@gmail.com>
2022-06-29 10:47:58 +0300

    added-modal

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const btnOpen = document.getElementById('btnOpen');
+const btnClose = document.getElementById('btnClose');
+const modal = document.getElementById('modal');
+
+btnClose.addEventListener('click', () => {
+  modal.classList.add('modal--hidden');
+});
+
+btnOpen.addEventListener('click', () => {
+  modal.classList.remove('modal--hidden');
+});
